9ab05185018181a9eb1639a4bc7e22b187056b0f
FGtatsuro <fujiistorage@gmail.com>
2016-03-02 00:35:25 +0900

    Update spec. Different formula name can be used via environment variable.

--- spec/intellij_spec.rb
+++ spec/intellij_spec.rb
@@ -1,5 +1,7 @@
 require "spec_helper_#{ENV['SPEC_TARGET_BACKEND']}"
 
-describe package('intellij-idea-ce'), :if => os[:family] == 'darwin' do
+formula = ENV['INTELLIJ_HOMEBREW_FORMULA'] || 'intellij-idea-ce'
+
+describe package(formula), :if => os[:family] == 'darwin' do
   it { should be_installed.by('homebrew_cask') }
 end
